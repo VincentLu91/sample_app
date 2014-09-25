f9a7af54365173794ea8a37ed196f80a2dc90ebb
Vince Lu <vincentlu@hotmail.ca>
2014-09-25 14:21:13 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name    "Vince Lu"
+        email   "vincelu@blah.com"
+        password "foobar"
+        password_confirmation   "foobar"
+    end
+end    
