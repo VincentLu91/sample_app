d7f38c28f613f4b8d78e2bb6f15a31d05f9f8b1a
Vince Lu <vincentlu@hotmail.ca>
2014-09-25 14:48:04 -0400

    Finish user signup AND fixed user name problem

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ describe "User pages" do
       let(:user) { FactoryGirl.create(:user) }
       before { visit user_path(user) }
       
-      #it { should have_content(user.name) }
+      it { should have_content(user.name) }
       it { should have_title(user.name) }
   end      
 
